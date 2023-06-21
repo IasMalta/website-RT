51646fe1f08ead1ee141ad0945884c922d710d05
iaswee <iasmiin.malta@gmail.com>
2023-06-21 14:54:15 -0300

    imagem menor slide e estilo

--- js/app.js
+++ js/app.js
@@ -30,14 +30,14 @@ window.onscroll = () => {
   contactInfo.classList.remove('active')
 }
 
-var radio = document.querySelector('.manual-btn')
+/*Var radio = document.querySelector('.manual-btn')
 var cont = 1
 
 document.getElementById('radio1').checked = true
 
 setInterval(() => {
   proximaImg()
-}, 4000)
+}, 6000)
 
 function proximaImg() {
   cont++
@@ -67,3 +67,20 @@ var swiper = new Swiper('.home-slider', {
     delay: 5000
   }
 }) */
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  autoplay: {
+    delay: 5000
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+})
